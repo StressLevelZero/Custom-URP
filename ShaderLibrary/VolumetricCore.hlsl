671ffbde72cbaaab83fad5ec044f6fb68b7cbda5
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2022-06-13 14:19:24 -0700

    Added volumetric functions for various blend modes

--- ShaderLibrary/VolumetricCore.hlsl
+++ ShaderLibrary/VolumetricCore.hlsl
@@ -15,10 +15,8 @@ float3 _VolCameraPos;
 TEXTURE3D(_VolumetricResult); SAMPLER(sampler_linear_clamp);
 float4 _VBufferDistanceEncodingParams;
 
-half4 Volumetrics(half4 color, half3 positionWS) {
-
-#if defined(_VOLUMETRICS_ENABLED)
-
+half4 GetVolumetricColor(float3 positionWS)
+{
     //float2 positionNDC = ComputeNormalizedDeviceCoordinates(positionWS, _PrevViewProjMatrix);//viewProjMatrix
 
     half4 ls = half4(positionWS - _VolCameraPos, -1); //_WorldSpaceCameraPos
@@ -28,15 +26,15 @@ half4 Volumetrics(half4 color, half3 positionWS) {
 
     float vdistance = distance(positionWS, _VolCameraPos);
 
-   // vdistance = LinearEyeDepth(vdistance, GetWorldToViewMatrix());
+    // vdistance = LinearEyeDepth(vdistance, GetWorldToViewMatrix());
 
     float W = EncodeLogarithmicDepthGeneralized(vdistance, _VBufferDistanceEncodingParams);
 
-     half halfU = ls.x * 0.5;
-   // half halfU = positionNDC.x * 0.5;
+    half halfU = ls.x * 0.5;
+    // half halfU = positionNDC.x * 0.5;
 
-    //Figuring out both sides at once and zeroing out the other when blending. 
-    //Is this better than branching with an if statement? Andorid doesn't like if statements anyway.
+     //Figuring out both sides at once and zeroing out the other when blending. 
+     //Is this better than branching with an if statement? Andorid doesn't like if statements anyway.
     half3 LUV = half3 (halfU.x, ls.y, W) * (1 - unity_StereoEyeIndex); //Left UV
     half3 RUV = half3(halfU + 0.5, ls.y, W) * (unity_StereoEyeIndex); //Right UV
     half3 DoubleUV = LUV + RUV; // Combined
@@ -45,14 +43,67 @@ half4 Volumetrics(half4 color, half3 positionWS) {
     //float ClipUVW =
     //    step(DoubleUV.x, 1) * step(0, DoubleUV.x) *
     //    step(DoubleUV.y, 1) * step(0, DoubleUV.y) ;
-    
-//    float random = GenerateHashedRandomFloat(DoubleUV * 4000) * 0.003;
-    half4 FroxelColor = SAMPLE_TEXTURE3D_LOD(_VolumetricResult, sampler_linear_clamp, DoubleUV,0) ;// *ClipUVW;
- //   half4 FroxelColor = SAMPLE_TEXTURE3D(_VolumetricResult, sampler_VolumetricResult, float3(positionNDC,W) ) ;// *ClipUVW;
 
+//    float random = GenerateHashedRandomFloat(DoubleUV * 4000) * 0.003;
+    return SAMPLE_TEXTURE3D_LOD(_VolumetricResult, sampler_linear_clamp, DoubleUV, 0);
+}
+
+half4 Volumetrics(half4 color, float3 positionWS) {
+
+#if defined(_VOLUMETRICS_ENABLED)
+
+    half4 FroxelColor = GetVolumetricColor(positionWS);
     color.rgb = FroxelColor.rgb + (color.rgb * FroxelColor.a);
 
-//    color.rgb = ls.xyz;
+#endif
+    return color;
+}
+
+half4 VolumetricsAlphaBlend(half4 color, half3 positionWS)
+{
+
+#if defined(_VOLUMETRICS_ENABLED)
+
+    half4 FroxelColor = GetVolumetricColor(positionWS);
+    //color.rgb = FroxelColor.rgb + (color.rgb * FroxelColor.a);
+    color.a = color.a * FroxelColor.a;
+#endif
+    return color;
+}
+
+half4 VolumetricsAdditive(half4 color, float3 positionWS)
+{
+
+#if defined(_VOLUMETRICS_ENABLED)
+
+    half4 FroxelColor = GetVolumetricColor(positionWS);
+    color.rgb = color.rgb * (1.0h - FroxelColor.a);
+
+#endif
+    return color;
+}
+
+
+half4 VolumetricsMultiplicative(half4 color, float3 positionWS)
+{
+
+#if defined(_VOLUMETRICS_ENABLED)
+
+    half4 FroxelColor = GetVolumetricColor(positionWS);
+    color.rgb = lerp(color.rgb, 1.0h, FroxelColor.a);
+
+#endif
+    return color;
+}
+
+half4 VolumetricsMultiplicative2(half4 color, float3 positionWS)
+{
+
+#if defined(_VOLUMETRICS_ENABLED)
+
+    half4 FroxelColor = GetVolumetricColor(positionWS);
+    color.rgb = lerp(color.rgb, 0.5h, FroxelColor.a);
+
 #endif
     return color;
 }
