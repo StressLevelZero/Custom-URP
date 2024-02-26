40917b3a979b3a1813f58bea80390871fa9c4f84
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2024-02-26 14:36:40 -0800

    Made quality setter method public, removed script that auto changes the quality setting when the build platform changes

--- ModResources/PlatformQualitySetter.cs
+++ ModResources/PlatformQualitySetter.cs
@@ -9,14 +9,14 @@ using Object = UnityEngine.Object;
 
 namespace SLZ.URPModResources
 {
-    internal static class PlatformQualitySetter
+    public static class PlatformQualitySetter
     {
         const string ProjectQSPath = "ProjectSettings/QualitySettings.asset";
         const string PcPresetPath = "Packages/com.unity.render-pipelines.universal/Presets/QualitySettings_PC.preset";
         const string QuestPresetPath = "Packages/com.unity.render-pipelines.universal/Presets/QualitySettings_Quest.preset";
         static readonly int[] questSettingsIndex = { 3 };
         static readonly int[] standaloneSettingsIndex = { 2, 1, 0 };
-        internal static void OverrideQualitySettings(BuildTarget target)
+        public static void OverrideQualitySettings(BuildTarget target)
         {
 
             Object oldQS = AssetDatabase.LoadAllAssetsAtPath(ProjectQSPath)[0];
