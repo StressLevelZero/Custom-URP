8e2dff067cd783dcf0429e708a81f5a4aba0b261
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2024-08-15 16:43:41 -0700

    Built new version of DXC compiler with optimizations for the input interface removed to align with what unity expects

--- Editor/UnityPatcher/DXC_Patch/CheckUnityInstall.cs
+++ Editor/UnityPatcher/DXC_Patch/CheckUnityInstall.cs
@@ -47,10 +47,12 @@ namespace SLZ.EditorPatcher
             {
                 bool warnNonWin = EditorUtility.DisplayDialog($"DXC update requested",
                       "The DirectX Shader Compiler needs to be manually updated to support Quest. " +
-                      "On non-windows platforms, this is not set up to update manually. " +
-                      "If you wish to use the DXC Compiler, download release 1.8.2407 from Microsoft's DXC github " +
+                      "On non-windows platforms, this is not set up to update manually as this package does not contain pre-compiled binaries. " +
+                      "If you wish to use the DXC Compiler, clone release-1.8.2407 from Microsoft's DXC github " +
                       "(https://github.com/microsoft/DirectXShaderCompiler/releases/tag/v1.8.2407). " +
-                      "Replace libdxcompiler.so and libdxil.so in (UnityEditorDir)/data/tools. " +
+                      "Apply SPIRV-Tools-unityfix.patch to external/SPIRV-Tools and build:\n" +
+                      $"{Path.Combine(Path.GetFullPath("Packages/com.unity.render-pipelines.universal/Editor/UnityPatcher/DXC_Patch/dxc~"), "SPIRV-Tools-unityfix.patch")}\n" +
+                      "Replace libdxcompiler.so in your editor install with the one you've built. " +
                       "Additionally, in your project locate Packages/com.stresslevelzero.urpconfig/include/DXCUpdateState.hlsl and uncomment '#define SLZ_DXC_UPDATED' " +
                       "to enable DXC compilation on Quest and enable other DXC features",
                       "Dismiss",
@@ -68,14 +70,14 @@ namespace SLZ.EditorPatcher
             string unity = EditorApplication.applicationPath;
             string toolsDir = Path.Combine(Path.GetDirectoryName(unity), "Data", "Tools");
             string unityDxcPath = Path.Combine(toolsDir, "dxcompiler.dll");
-            string unityDxilPath = Path.Combine(toolsDir, "dxil.dll");
-            bool unityDXCExists = File.Exists(unityDxcPath) && File.Exists(unityDxilPath);
+            // string unityDxilPath = Path.Combine(toolsDir, "dxil.dll");
+            bool unityDXCExists = File.Exists(unityDxcPath) /* && File.Exists(unityDxilPath) */;
             FileVersionInfo installDXCVersion = unityDXCExists ? FileVersionInfo.GetVersionInfo(unityDxcPath) : null;
 
             string localPath = Path.GetFullPath("Packages/com.unity.render-pipelines.universal/Editor/UnityPatcher/DXC_Patch/dxc~");
             string localDxcPath = Path.Combine(localPath, "dxcompiler.dll");
-            string localDxilPath = Path.Combine(localPath, "dxil.dll");
-            bool localDXCExists = File.Exists(localDxcPath) && File.Exists(localDxilPath);
+            //string localDxilPath = Path.Combine(localPath, "dxil.dll");
+            bool localDXCExists = File.Exists(localDxcPath) /* && File.Exists(localDxilPath) */;
             FileVersionInfo localDXCVersion = localDXCExists ? FileVersionInfo.GetVersionInfo(localDxcPath) : null;
 
             if (!localDXCExists || !unityDXCExists)
@@ -92,10 +94,10 @@ namespace SLZ.EditorPatcher
                     {
                         EditorUtility.DisplayDialog("DXC Update Failed", string.Format("ABORTING: DXC update failed.\n{0}{1}{2}" +
                             "\nTo prevent corrupting the cache server, unity will now close",
-                            !unityDXCExists ? $"Could not find unity's DXC dlls at:\n{unityDxcPath}\n{unityDxilPath}\n" : "",
-                            !localDXCExists ? $"Could not find replacement dlls at:\n{localDxcPath}\n{localDxilPath}\n" : "",
+                            !unityDXCExists ? $"Could not find unity's DXC dlls at:\n{unityDxcPath}\n" : "",
+                            !localDXCExists ? $"Could not find replacement dlls at:\n{localDxcPath}\n" : "",
                             needsUpdate && unityDXCExists ? $"Unity's DXC was found and out of date (Found {installDXCVersion.FileMajorPart}.{installDXCVersion.FileMinorPart}, expected {defaultNewDXCVersionMajor}.{defaultNewDXCVersionMinor})\n" +
-                            $"Try manually updating {unityDxcPath} and dxil.dll with https://github.com/microsoft/DirectXShaderCompiler/releases/tag/v1.8.2407\n" : ""
+                            $"Try manually replacing {unityDxcPath} with {localDxcPath}\n" : ""
                             ), "Exit");
                     }
                     Instagib();
@@ -113,12 +115,12 @@ namespace SLZ.EditorPatcher
                     Debug.LogError("\n\nWARNING! The URP is going to attempt to modify the Unity install! The DXC shader compiler needs to be updated " +
                         $"to support multiview on Quest. Found version {installDXCVersion.ProductVersion}, working version is {localDXCVersion.ProductVersion}" +
                         "This will exit Unity and attempt to replace the following files:\n" +
-                        $"{unityDxcPath}\n{unityDxilPath}\n" +
+                        $"{unityDxcPath}\n" +
                         "with more up-to-date versions found at:\n" +
-                        $"{localDxcPath}\n{localDxilPath}\n" +
+                        $"{localDxcPath}\n" +
                         $"The old DLLs will be moved to a folder named \"DXC_Backup\" in the unity folder.\n" +
                         "This will almost certainly fail if other editor instances are running! In the event that it does, manually backup unity's dlls and " +
-                        "overwrite them with the new dlls from this package. You may also obtain the dlls from https://github.com/microsoft/DirectXShaderCompiler/releases . " +
+                        "overwrite them with the new dlls from this package. You may also build the dlls from https://github.com/microsoft/DirectXShaderCompiler/releases . " +
                         "This is tested working with the June 2024 release (1.8.2407), later releases may be incompatible!\n\n"
                         );
                     choice = 0;
@@ -128,7 +130,7 @@ namespace SLZ.EditorPatcher
 
                     choice = EditorUtility.DisplayDialogComplex($"DXC update requested ({installDXCVersion.ProductVersion}->{localDXCVersion.ProductVersion})",
                         "The DirectX Shader Compiler needs to be updated to support Quest. Allow Update?\n\n" +
-                        "WARNING: This will replace dxcompiler.dll and dxil.dll in your Unity Editor install, affecting all projects on this unity version. " +
+                        "WARNING: This will replace dxcompiler.dll in your Unity Editor install, affecting all projects on this unity version. " +
                         "Backups of the original dlls can be found in Editor/Data/Tools/DXC_Backup.\n\n" +
                         "If this is not updated, Quest will instead fall back to the FXC compiler.\n\n" +
                         "Before updating, close all other unity editor applications.",
@@ -143,12 +145,12 @@ namespace SLZ.EditorPatcher
                 try
                 {
 
-                    bool updateSuccess = UpdateDXC(localDxcPath, localDxilPath, unityDxcPath, unityDxilPath, out string message);
+                    bool updateSuccess = UpdateDXC(localDxcPath, unityDxcPath, out string message);
                     if (!updateSuccess)
                     {
                         string errMsg = $"DXC Update Failed:\n{message}\n\n" +
                                 "Close any other instance of the editor, and kill any remaining unity or unityshadercompiler processes from task manager!\n" +
-                                $"You may also try manually moving these files:\n{localDxcPath}\n{localDxilPath}\nTo:\n{unityDxcPath}\n{unityDxilPath}\n\n" +
+                                $"You may also try manually moving these files:\n{localDxcPath}\nTo:\n{unityDxcPath}\n\n" +
                                 "Aborting!";
                         Debug.LogError(message);
                         if (!Application.isBatchMode)
@@ -204,13 +206,13 @@ namespace SLZ.EditorPatcher
             SessionState.SetBool("DXCChecked", true);
         }
 
-        static bool UpdateDXC(string inDXCPath, string inDXILPath, string outDXCPath, string outDXILPath, out string errMsg)
+        static bool UpdateDXC(string inDXCPath, string outDXCPath, /*string outDXILPath,*/ out string errMsg)
         {
             //StringBuilder errBuilder = new StringBuilder();
             string backupPath = Path.Combine(Path.GetDirectoryName(outDXCPath), "DXC_Backup");
             Directory.CreateDirectory(backupPath);
             string backupDXC = Path.Combine(backupPath, "dxcompiler.dll");
-            string backupDXIL = Path.Combine(backupPath, "dxil.dll");
+            //string backupDXIL = Path.Combine(backupPath, "dxil.dll");
 
             if (!File.Exists(backupDXC))
             {
@@ -231,23 +233,23 @@ namespace SLZ.EditorPatcher
                 Debug.Log($"dxcompiler.dll already found at {backupDXC}, skipping backup");
             }
 
-            if (!File.Exists(backupDXIL))
-            {
-                try
-                {
-                    Debug.Log($"Backing up {outDXILPath} to {backupDXIL}");
-                    File.Copy(outDXILPath, backupDXIL);
-                }
-                catch (Exception ex)
-                {
-                    errMsg = $"Failed to backup dxil.dll, aborting: {ex.Message}";
-                    return false;
-                }
-            }
-            else
-            {
-                Debug.Log($"dxil.dll already found at {backupDXIL}, skipping backup");
-            }
+            //if (!File.Exists(backupDXIL))
+            //{
+            //    try
+            //    {
+            //        Debug.Log($"Backing up {outDXILPath} to {backupDXIL}");
+            //        File.Copy(outDXILPath, backupDXIL);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        errMsg = $"Failed to backup dxil.dll, aborting: {ex.Message}";
+            //        return false;
+            //    }
+            //}
+            //else
+            //{
+            //    Debug.Log($"dxil.dll already found at {backupDXIL}, skipping backup");
+            //}
 
             try
             {
@@ -259,34 +261,35 @@ namespace SLZ.EditorPatcher
                 errMsg = $"Failed to overwrite dxcompiler.dll, aborting: {ex.Message}";
                 return false;
             }
-            try
-            {
-                Debug.Log($"Ovewriting {outDXILPath} with {inDXILPath}");
-                File.Copy(inDXILPath, outDXILPath, true);
-            }
-            catch (Exception ex)
-            {
-                errMsg = $"Failed to overwrite dxil.dll, attempting to restore dxcompiler.dll and aborting: {ex.Message}";
-                try
-                {
-                    File.Copy(backupDXC, outDXCPath, true);
-                }
-                catch (Exception ex2)
-                {
-                    errMsg += $"\nFailed to restore dxcompiler.dll!: {ex2.Message}";
-                }
-                return false;
-            }
 
-            /* Update via cmd, less safe but allows us to copy the files after the editor has closed
-            string pause = Application.isBatchMode ? "" : "& pause";
-            Process cmd = new Process();
-            cmd.StartInfo.FileName = "cmd.exe";
-            cmd.StartInfo.Arguments = $"/c timeout /t 5 & copy /b/v/y \"{inDXCPath}\" \"{outDXCPath}\" & copy /b/v/y \"{inDXILPath}\" \"{outDXILPath}\" {pause}";
-            cmd.StartInfo.UseShellExecute = true;
-            cmd.StartInfo.CreateNoWindow = false;
-            cmd.Start();
-            */
+            //try
+            //{
+            //    Debug.Log($"Ovewriting {outDXILPath} with {inDXILPath}");
+            //    File.Copy(inDXILPath, outDXILPath, true);
+            //}
+            //catch (Exception ex)
+            //{
+            //    errMsg = $"Failed to overwrite dxil.dll, attempting to restore dxcompiler.dll and aborting: {ex.Message}";
+            //    try
+            //    {
+            //        File.Copy(backupDXC, outDXCPath, true);
+            //    }
+            //    catch (Exception ex2)
+            //    {
+            //        errMsg += $"\nFailed to restore dxcompiler.dll!: {ex2.Message}";
+            //    }
+            //    return false;
+            //}
+
+            // // Update via cmd, less safe but allows us to copy the files after the editor has closed
+            // string pause = Application.isBatchMode ? "" : "& pause";
+            // Process cmd = new Process();
+            // cmd.StartInfo.FileName = "cmd.exe";
+            // cmd.StartInfo.Arguments = $"/c timeout /t 5 & copy /b/v/y \"{inDXCPath}\" \"{outDXCPath}\" & copy /b/v/y \"{inDXILPath}\" \"{outDXILPath}\" {pause}";
+            // cmd.StartInfo.UseShellExecute = true;
+            // cmd.StartInfo.CreateNoWindow = false;
+            // cmd.Start();
+            
             errMsg = "No error";
             return true;
         }
--- ShaderLibrary/SLZVkExtensions.hlsl
+++ ShaderLibrary/SLZVkExtensions.hlsl
@@ -16,22 +16,21 @@
 	
 	// ABSOLUTELY BUSTED. Even when requesting a SM6.6 feature and using the proper HLSL SV_ShadingRate, simply adding the SPV_KHR_fragment_shading_rate extension causes
 	// the unity projection matrices to become garbled. Comparing the compiled shader outputs with and without SPV_KHR_fragment_shading_rate, DXC somehow decides to not
-	// optimize out any unused resource if SPV_KHR_fragment_shading_rate is requested. Unity is probably getting really confused and binding stuff at the wrong location.
+	// optimize out any unused resources if SPV_KHR_fragment_shading_rate is requested. Unity is probably getting really confused and binding stuff at the wrong location.
 	// Or DXC is simply producing invalid code
-	
 	
 	// Fragment Shading Rate - Theoretically could be used to output per primitive shading rate. 
 	// Needs cooperation from the Quest/VRS rendering plugins to set the pipeline combiner ops to
 	// take the max with the pipeline and image level rates (currently doing KEEP op, which overwrites with pipeline rate).
-	// #define SLZ_REQUEST_SHADING_RATE_CAPS        [[vk::ext_extension("SPV_KHR_fragment_shading_rate")]] \
+	#define SLZ_REQUEST_SHADING_RATE_CAPS        [[vk::ext_extension("SPV_KHR_fragment_shading_rate")]] \
                                                  [[vk::ext_capability(/*FragmentShadingRateKHR*/ 4422)]]
 	// Write only, only valid in vertex and geo stages.
-	// #define SLZ_DECLARE_PRIMITIVE_SHADING_RATE , [[vk::ext_decorate(/*Builtin*/ 11, /*PrimitiveShadingRateKHR*/ 4432)]] out uint PrimitiveShadingRate : PRIMRATE
-	// #define SLZ_SET_PRIMITIVE_SHADING_RATE(value) PrimitiveShadingRate = value;
+	#define SLZ_DECLARE_PRIMITIVE_SHADING_RATE , [[vk::ext_decorate(/*Builtin*/ 11, /*PrimitiveShadingRateKHR*/ 4432)]] out uint PrimitiveShadingRate : PRIMRATE
+	#define SLZ_SET_PRIMITIVE_SHADING_RATE(value) PrimitiveShadingRate = value;
 	
 	
 	// Combined image sampler - unity can't figure out how to bind this.
-	// #define DECLARE_COMBINED_SAMPLER(_register_) [[vk::combinedImageSampler]][[vk::binding(_register_)]]
+	#define DECLARE_COMBINED_SAMPLER(_register_) [[vk::combinedImageSampler]][[vk::binding(_register_)]]
 #else
 	// Fragment Invocation Density
 	#define SLZ_REQUEST_FRAG_SIZE_CAPS
