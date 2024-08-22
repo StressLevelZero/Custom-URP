30cbca32babd81f22328812633268e0a5d7552b0
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2024-08-22 13:27:45 -0700

    meaningless change to invalidate shader cache

--- ShaderLibrary/PlatformCompiler.hlsl
+++ ShaderLibrary/PlatformCompiler.hlsl
@@ -4,6 +4,7 @@
 #include "Packages/com.stresslevelzero.urpconfig/include/DXCUpdateState.hlsl"
 
 #if defined(SLZ_DXC_UPDATED) || defined(SHADER_API_DESKTOP)
+#define FORCE_NEW_ARTIFACTS 1
 #pragma use_dxc vulkan
 #endif
 
