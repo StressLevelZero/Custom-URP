a42faf5d25b69a1a6f3f40340f3fa8bcbafe321d
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2024-09-30 19:16:30 -0700

    Added script to murder unity if its open while the URP or core are being updated

---
+++ Editor/UnityPatcher/CachePatch/EndUnityIfPipelineUpdates.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+public static class EndUnityIfPipelineUpdates
+{
+    [InitializeOnLoadMethod]
+    static void CheckOrDie()
+    {
+        var urpPkgInfo = UnityEditor.PackageManager.PackageInfo.FindForPackageName("com.unity.render-pipelines.universal");
+        var corePkgInfo = UnityEditor.PackageManager.PackageInfo.FindForPackageName("com.unity.render-pipelines.core");
+
+        string currentUrpHash = urpPkgInfo.git != null ? urpPkgInfo.git.hash : "0";
+        string currentCoreHash = corePkgInfo.git != null ? corePkgInfo.git.hash : "0";
+        Debug.Log($"URP Git Hash: {currentUrpHash}");
+        Debug.Log($"SRP Core Git Hash: {currentCoreHash}");
+        string oldUrpHash = SessionState.GetString("URPHash", string.Empty);
+        string oldCoreHash = SessionState.GetString("SRPCoreHash", string.Empty);
+        if (string.IsNullOrEmpty(oldUrpHash))
+        {
+            Debug.Log($"URP Git Hash: {currentUrpHash}"); 
+            SessionState.SetString("URPHash", currentUrpHash);
+            oldUrpHash = currentUrpHash;
+        }
+        if (string.IsNullOrEmpty(oldCoreHash))
+        {
+            Debug.Log($"SRP Core Git Hash: {currentCoreHash}");
+            SessionState.SetString("SRPCoreHash", currentCoreHash);
+            oldCoreHash = currentCoreHash;
+        }
+
+        if (!string.Equals(oldUrpHash, currentUrpHash) || !string.Equals(oldCoreHash, currentCoreHash))
+        {
+            Debug.LogError("PANIC - URP or Core pipelines updated while unity was open! Force closing unity!");
+            Instagib();
+        }
+    }
+
+    static void Instagib()
+    {
+        Debug.LogWarning("Killing Unity immediately");
+        int procID = Process.GetCurrentProcess().Id;
+        Process cmd = new Process();
+        cmd.StartInfo.FileName = "taskkill";
+        cmd.StartInfo.Arguments = $"/F /PID {procID}";
+        cmd.StartInfo.UseShellExecute = false;
+        cmd.Start();
+    }
+}
--- ShaderLibrary/PlatformCompiler.hlsl
+++ ShaderLibrary/PlatformCompiler.hlsl
@@ -4,7 +4,7 @@
 #include "Packages/com.stresslevelzero.urpconfig/include/DXCUpdateState.hlsl"
 
 #if defined(SLZ_DXC_UPDATED) || defined(SHADER_API_DESKTOP)
-#define FORCE_NEW_ARTIFACTS 2
+#define FORCE_NEW_ARTIFACTS 3
 #pragma use_dxc vulkan
 #endif
 
