a9c07265ac591bc0355e62cf9fc998028ee2f31e
KevinComerford <comerfordkevin@gmail.com>
2024-10-28 19:23:29 -0700

    Added SkyOcclusionProbesEditor
    Added editor to more easily place and edit Sky Occlusion Probes

--- Runtime/Volumetrics/SkyOcclusionProbes.cs
+++ Runtime/Volumetrics/SkyOcclusionProbes.cs
@@ -64,31 +64,31 @@ public class SkyOcclusionProbes : MonoBehaviour
         // }
     }
 
-    void drawTetrahedron(Tetrahedron tet, Vector3[] pos, Color color)
-    {
-        Vector3 v0 = pos[tet.Vert0]
-            , v1 =   pos[tet.Vert1]
-            , v2 =   pos[tet.Vert2]
-            , v3 =   pos[tet.Vert3];
-        
-        Gizmos.color = color;
-        Gizmos.DrawLine(v0, v1);
-        Gizmos.DrawLine(v0, v2);
-        Gizmos.DrawLine(v0, v3);
-        Gizmos.DrawLine(v1, v2);
-        Gizmos.DrawLine(v1, v3);
-        Gizmos.DrawLine(v2, v3);
-    }
-    
-    static void drawString(string text, Vector3 worldPos, Color? colour = null) {
-        UnityEditor.Handles.BeginGUI();
-        if (colour.HasValue) GUI.color = colour.Value;
-        var view = UnityEditor.SceneView.currentDrawingSceneView;
-        Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
-        Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
-        GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
-        UnityEditor.Handles.EndGUI();
-    }
+    // void drawTetrahedron(Tetrahedron tet, Vector3[] pos, Color color)
+    // {
+    //     Vector3 v0 = pos[tet.Vert0]
+    //         , v1 =   pos[tet.Vert1]
+    //         , v2 =   pos[tet.Vert2]
+    //         , v3 =   pos[tet.Vert3];
+    //     
+    //     Gizmos.color = color;
+    //     Gizmos.DrawLine(v0, v1);
+    //     Gizmos.DrawLine(v0, v2);
+    //     Gizmos.DrawLine(v0, v3);
+    //     Gizmos.DrawLine(v1, v2);
+    //     Gizmos.DrawLine(v1, v3);
+    //     Gizmos.DrawLine(v2, v3);
+    // }
+    //
+    // static void drawString(string text, Vector3 worldPos, Color? colour = null) {
+    //     UnityEditor.Handles.BeginGUI();
+    //     if (colour.HasValue) GUI.color = colour.Value;
+    //     var view = UnityEditor.SceneView.currentDrawingSceneView;
+    //     Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
+    //     Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
+    //     GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
+    //     UnityEditor.Handles.EndGUI();
+    // }
     private void Awake()
     {
        if (skyOcclusionDataAsset!=null) VolumetricRegisters.RegisterSkyOcclusionProbes(this);
