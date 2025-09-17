ac221e58adf1ef3bbf49d70e329d1955c5ffc0c3
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2025-09-17 15:35:39 -0700

    Add script to automate tarballing this package and increment the package version number

--- CommitAndPush.sh
+++ CommitAndPush.sh
@@ -1,5 +1,8 @@
+#printf "$@"
 git show -s --format=%H > prevGitCommitHash.txt
-git add ./PreviousGitCommitHash.txt
-git commit -m $1
+git add ./prevGitCommitHash.txt
+./../IncrementVersionNumber.exe package.json
+git add ./package.json
+git commit -m "$@"
 git archive --format=tar.gz -o ./../com.unity.render-pipelines.universal.tar.gz HEAD
 git push
