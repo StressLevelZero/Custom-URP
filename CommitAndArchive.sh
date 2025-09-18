bb485fb92b7e131b0b31381c42c0fde290c52692
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2025-09-17 18:19:19 -0700

    "Fixed issue with bash script producing package archive that unity couldn't load, made CommitAndArchive significantly more advanced with arguments to disable parts"

--- CommitAndArchive.sh
+++ CommitAndArchive.sh
@@ -1,7 +1,64 @@
-#printf "$@"
-git show -s --format=%H > prevGitCommitHash.txt
-git add ./prevGitCommitHash.txt
-./../../Tools/IncrementVersionNumber/IncrementVersionNumber.exe package.json
-git add ./package.json
-git commit -m "$@"
-git archive --format=tar.gz -o ./../../RenderingPackageArchives/com.unity.render-pipelines.universal.tar.gz HEAD
+PACKAGE_NAME=com.unity.render-pipelines.universal
+
+if [[ $# == 0 ]]; then
+    echo "USAGE: ./CommitAndArchive.sh [OPTIONS] \"Commit message\""
+    echo "Increments the package version number, dumps the current commit hash to a "
+    echo "file, commits the pending changes, and archives the project to a tarball"
+    echo "suitable for use with Unity Package Manager as a \"local-tarball\" type "
+    echo "package"
+    echo "Options:"
+    echo "    -i     Skip incrementing the version number"
+    echo "    -h     Skip dumping the commit hash to prevGitCommitHash.txt"
+    echo "    -c     Skip commiting the changes"
+    echo "    -a     Skip archiving the package to a tarball"
+    echo "    -p     Push the commit"
+    exit
+fi
+
+while getopts 'ihcap' opt; do
+    case $opt in
+        i) 
+        NO_INCREMENT="SET"
+        ;;
+        h) NO_HASH="SET"
+        ;;
+        c) NO_COMMIT="SET"
+        ;;
+        a) NO_ARCHIVE="SET"
+        ;;
+        p) PUSH_COMMIT="SET"
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+COMMIT_MESSAGE="$@"
+
+if [[ ! -v NO_COMMIT ]] && [[ -z "${COMMIT_MESSAGE}" ]]; then
+    "Missing commit message"
+    exit
+fi
+
+# Dump current commit hash to a file
+if [[ ! -v NO_HASH ]]; then
+    git show -s --format=%H > prevGitCommitHash.txt
+    git add ./prevGitCommitHash.txt
+fi
+
+# Run a C# script in the RenderPipelines project to increment the package.json minor version number
+if [[ ! -v NO_INCREMENT ]]; then
+    ./../../Tools/IncrementVersionNumber/IncrementVersionNumber.exe package.json
+    git add ./package.json
+fi
+
+# Commit the pending changes with the arguments to this script as the commit message
+if [[ ! -v NO_COMMIT ]]; then
+    git commit -m "\"$COMMIT_MESSAGE\""
+fi
+
+if [[ ! -v NO_ARCHIVE ]]; then
+    git archive --format=tar.gz --prefix="package/" -o ./../../RenderingPackageArchives/$PACKAGE_NAME.tar.gz HEAD
+fi
+
+if [[ -v PUSH_COMMIT ]]; then
+    git push
+fi
