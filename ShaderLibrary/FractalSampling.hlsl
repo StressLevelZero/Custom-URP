84af86a2a275bcb2c22bdea49cc0b924a2ad4f22
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2026-04-10 19:23:49 -0700

    Updated the fractal sampling function to reduce complexity and register pressure. Replaced the lengthy LOD calculation with the builtin CalculateLevelOfDetail function, which has the added benefit of being quad-uniform so manual derivatives are no longer necessary.

--- ShaderLibrary/FractalSampling.hlsl
+++ ShaderLibrary/FractalSampling.hlsl
@@ -4,73 +4,72 @@
 
 // Returns something proportional to "how minified is this texture lookup right now?"
 // Uses texel-space derivatives so it automatically adapts to UV tiling + object scale + texture resolution.
-float ComputeFractalDepth(float2 uv, Texture2D tex)
+half ComputeFractalDepth(float2 uv, Texture2D tex)
 {
-    float _FractalDepthMul = 1.; // start with 1.0
-
-    float2 texel_size = 0;
-    uint MipLevel=0;  uint Width=128;   uint Height=128;	uint NumberOfLevels=0;
-    tex.GetDimensions( MipLevel,Width,Height,NumberOfLevels);
-    texel_size.x = Width;
-    texel_size.y = Height;
+    half2 texel_size = 0;
+    tex.GetDimensions(texel_size.x,texel_size.y);
     
     // texels per pixel (roughly)
-    float2 duvdx = ddx(uv) * texel_size;
-    float2 duvdy = ddy(uv) * texel_size;
+    half2 duvdx = half2(ddx(uv) * texel_size);
+    half magDdx = dot(duvdx, duvdx);
+    half2 duvdy = half2(ddy(uv) * texel_size);
+    half magDdy = dot(duvdy, duvdy);
 
-    float rho = max(dot(duvdx, duvdx), dot(duvdy, duvdy)); // (texels/pixel)^2
-    float footprint = sqrt(max(rho, 1e-12));               // texels/pixel
-
+    half rho = max(magDdx, magDdy); // (texels/pixel)^2
+    half footprint = max(sqrt(rho), half(0.00006103515626)); // texels/pixel
     // Map into the "depth" space expected by FractalTextureMip (must be > 0)
-    return max(footprint * _FractalDepthMul, 1e-6);
+    return footprint;// max(footprint, 1e-6);
 }
 
-
+static min16uint2 GLOBAL_QUAD_INDEX = min16uint2(0, 0);
 
 // Unity/HLSL port of XorDev's original "fractal_texture_mip" (same math: log/exp are natural base-e)
 //
 // NOTE: Must be called in a pixel/fragment shader (ddx/ddy/derivatives required).
 
-float4 FractalTextureMip(Texture2D tex, SamplerState samp, float2 uv, float depth)
+float4 FractalTextureMip(Texture2D tex, SamplerState samp, float2 uv)
 {
-    depth = max(depth, 1e-18);
+    // Replace ComputeFractalDepth with the CalculateLevelOfDetail function. This is faster by virtue of
+    // giving us a quad-uniform value. This means there won't be a change in fractal detail scale within
+    // the quad, and the normal texture sample method can be used without having seams at fractal scale
+    // boundaries.
+    //
+    // This has a slight quirk in that the return value is clamped to [0, log2(resolution)]. In order to 
+    // get fractal details at scales below where mip 0 would be used, we just multiply the UV's by a power
+    // of 2 to offset the mip level to a positive number then subtract out the log2 of that number after. 
+    float LOD = tex.CalculateLevelOfDetail(samp, uv * 128) - 7.0;
 
-    // Find the pixel level of detail (natural log)
-    float LOD = log(depth);
+    // ComputeFractalDepth can still be used if we broadcast or average the LOD value within the quad.
+    // Note that Non uniform quad intrinsics (VK_KHR_shader_quad_control / OpCapability GroupNonUniformQuad) seem 
+    // to cause the Adreno compiler to run the shader at quarter rate? Claimed occupancy suddenly jumps from 50%
+    // to 100% when GroupNonUniformQuad is declared. This makes me very suspicious that quad intrinsics are 
+    // emulated in a way that drops the maximum number of threads per compute unit by 4. Thus the theoretical max
+    // occupancy is 4x lower and the impact of register use is also 4x lower.
 
-    // Round LOD down
-    float LOD_floor = floor(LOD);
+    #if 0 
+        float LOD = min(log2(ComputeFractalDepth(uv, tex)), 65503);
+        #if !defined(SHADER_API_MOBILE) 
+        LOD = QuadReadLaneAt(LOD, 0u);
+        #endif
+    #endif
+    
+    float2 uv1 = uv / exp2(floor(LOD));
 
-    // Fract part for interpolating
-    float LOD_fract = LOD - LOD_floor;
 
-    // Compute scaled uvs (natural exp)
-    float e1 = exp(LOD_floor - 1.0);
-    float e2 = exp(LOD_floor + 0.0);
-    float e3 = exp(LOD_floor + 1.0);
+    half4 tex0 = tex.Sample(samp, uv1 * 2.0f);
+    half4 tex2 = tex.Sample(samp, uv1 * 0.5f);
+    half4 blend = lerp(tex0, tex2, frac(LOD));
 
-    float2 uv1 = uv / e1;
-    float2 uv2 = uv / e2;
-    float2 uv3 = uv / e3;
+    half4 tex1 = tex.Sample(samp, uv1);
 
-    // Compute continuous derivatives (matches original)
-    float2 dx = ddx(uv) / depth * exp(1.0);
-    float2 dy = ddy(uv) / depth * exp(1.0);
-
-    // Sample at 3 scales
-    float4 tex0 = tex.SampleGrad(samp, uv1, dx, dy);
-    float4 tex1 = tex.SampleGrad(samp, uv2, dx, dy);
-    float4 tex2 = tex.SampleGrad(samp, uv3, dx, dy);
-
-    // Blend samples together
-    return (tex1 + lerp(tex0, tex2, LOD_fract)) * 0.5;
+    return (tex1 + blend) * half(0.5);
 }
 
 //Drop in replacement for SAMPLE_TEXTURE2D
 float4 SAMPLE_TEXTURE2D_FRACTAL(Texture2D tex, SamplerState ss,  float2 uv )
 {
-    float depth = ComputeFractalDepth(uv, tex);
-    return FractalTextureMip(tex, ss, uv, depth);
+    //float depth = ComputeFractalDepth(uv, tex);
+    return FractalTextureMip(tex, ss, uv);
 }
 
 //Shader Graph hook
