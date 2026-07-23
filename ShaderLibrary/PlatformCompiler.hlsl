784a1d2d4909d31522952a06b36f7339a874b86a
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2026-07-22 19:35:06 -0700

    Add case for disabling DXC to PlatformCompiler.hlsl

--- ShaderLibrary/PlatformCompiler.hlsl
+++ ShaderLibrary/PlatformCompiler.hlsl
@@ -3,9 +3,13 @@
 
 #include "Packages/com.stresslevelzero.urpconfig/include/DXCUpdateState.hlsl"
 
-#if defined(SLZ_DXC_UPDATED) || defined(SHADER_API_DESKTOP)
+#if (defined(SLZ_DXC_UPDATED) || defined(SHADER_API_DESKTOP)) && !defined(SLZ_NATIVE_PLUGIN_DISABLE_DXC)
 #define FORCE_NEW_ARTIFACTS 4
 #pragma use_dxc vulkan
+#endif
+
+#if defined(SLZ_NATIVE_PLUGIN_DISABLE_DXC)
+#pragma never_use_dxc
 #endif
 
 #endif
